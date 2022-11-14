d8e123f365bd73805f5f93983a8cfd0c8496e921
CaioHSCruz <caiohscruz@hotmail.com>
2022-11-14 16:49:32 -0300

    Implements Infra.Ioc Layer and cofigure Presentation Layer to Dependency Container

---
+++ CleanArch/CleanArch.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Ioc
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            #region Application Layer
+            services.AddScoped<CourseService, CourseService>();
+            #endregion
+
+            #region Infra.Data Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+
+            #endregion           
+        }
+    }
+}
--- CleanArch/CleanArch.MVC/Program.cs
+++ CleanArch/CleanArch.MVC/Program.cs
@@ -1,4 +1,5 @@
 using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.Ioc;
 using CleanArch.MVC.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+DependencyContainer.RegisterServices(builder.Services);
 
 var app = builder.Build();
 
